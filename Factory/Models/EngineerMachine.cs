4f363a06abd1a7768ee2222984da754489915eee
Henry Sullivan <sullivanbhenry@gmail.com>
2023-01-06 10:59:27 -0800

    can add and delete machines from the engineer side

--- Factory/Models/EngineerMachine.cs
+++ Factory/Models/EngineerMachine.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Factory.Models
 {
   public class EngineerMachine
   {
     public int EngineerMachineId { get; set; }
+    // [Range(1, int.MaxValue, ErrorMessage = "There must be a machine to assign to an engineer. Are any machines installed?")]
     public int MachineId { get; set; }
     public Machine Machine { get; set; }
     public int EngineerId { get; set; }

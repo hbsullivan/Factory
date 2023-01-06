ce571329d2a7d31dad927e1a41db1785ccc68644
Henry Sullivan <sullivanbhenry@gmail.com>
2023-01-06 15:01:07 -0800

    WIP: adding model validation

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -6,7 +6,6 @@ namespace Factory.Models
 {
   public class Machine
   {
-    // [Range(1, int.MaxValue, ErrorMessage = "There must be a machine to assign to an engineer. Are any machines installed?")]
     public int MachineId { get; set; }
     [Required(ErrorMessage = "The machine must have a name!")]
     public string Name { get; set; }
